30858e3e6999a2cdc36bc7674f8690c3a9a2aa8c
Arbz583 <bakhtiarialireza44@gmail.com>
2023-05-31 18:25:09 +0330

    TemplateView

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
 
 def indexView(request):
     return render(request, 'index.html', {'name':'Hasan'} )
+class IndexView(TemplateView):
+    template_name="index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)        #super idicate to parrent class (TemplateView)
+        context["name"] = 'Omid'
+        context["posts"]=Post.objects.all()
+        return context
+    
