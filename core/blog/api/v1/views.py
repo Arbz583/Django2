7d2f70aaf1e7b8f4f1e066c30e9a3374aebfd7ca
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-04 16:00:01 +0330

    status code

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,17 +2,36 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view()
 def postList(request):
     return Response('OK GUY!')
 
+
+# @api_view()
+# def postDetail(request, id):
+#     post=Post.objects.get(pk=id)
+#     print(post.__dict__)             #__dict__ showes all itemes in  the object
+#     serializer=PostSerializer(post)
+#     print(serializer.__dict__)
+#     print(serializer.data)
+#     return Response(serializer.data)
+
+# @api_view()
+# def postDetail(request, id):
+#     try:
+#         post=Post.objects.get(pk=id)
+#         serializer=PostSerializer(post)       
+#         return Response(serializer.data)
+#     except Post.DoesNotExist:
+#         return Response({'detail':'post does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
 @api_view()
 def postDetail(request, id):
-    post=Post.objects.get(pk=id)
-    print(post.__dict__)             #__dict__ showes all itemes in  the object
+    post=get_object_or_404(Post, pk=id)
     serializer=PostSerializer(post)
-    print(serializer.__dict__)
-    print(serializer.data)
     return Response(serializer.data)
+
