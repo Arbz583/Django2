0a1d9480bc340319a78f66a182432592ef16b654
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-06 18:24:25 +0330

    Inner func in serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,10 +9,16 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet=serializers.ReadOnlyField(source='get_snippet')
     relative_url=serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url=serializers.SerializerMethodField(method_name='getabs_url') #if you do not use method_name you should use get_ to define relative function
 
     class Meta:
         model=Post
-        fields=['id', 'author','title','content','snippet','category', 'status','relative_url', 'published_date' ]
+        fields=['id', 'author','title','content','snippet','category', 'status','relative_url','absolute_url', 'published_date' ]
+
+
+    def getabs_url(self, obj):
+        request=self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
