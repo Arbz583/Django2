bc782347f0ecd547027c4f9c2c5d126d55049443
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-06 09:43:11 +0330

    SimpleRouter

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,4 @@
-#from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly #, IsAdminUser=is staff
 from rest_framework.response import Response
 from .serializers import PostSerializer, CategorySerializer
@@ -136,6 +136,10 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class=PostSerializer     
     queryset=Post.objects.filter(status=True)   
+ 
+    @action(methods=['get'], detail=False)    #creating extra url with simple router. detail=false means no arg be sended in url.
+    def get_ok(self, request):
+        return Response({'detail':'ok'})
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
