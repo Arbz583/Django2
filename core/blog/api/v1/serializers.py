54801793371a6bd6cdb2e29eef34a1104e08979a
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-07 06:38:01 +0330

    Category with to_represtation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -14,7 +14,7 @@ class PostSerializer(serializers.ModelSerializer):
     snippet=serializers.ReadOnlyField(source='get_snippet')
     relative_url=serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url=serializers.SerializerMethodField(method_name='getabs_url') #if you do not use method_name you should use get_ to define relative function
-    category=CategorySerializer()     #serializers.SlugRelatedField(many=False,slug_field='name',queryset=Category.objects.all())
+    category=serializers.SlugRelatedField(many=False,slug_field='name',queryset=Category.objects.all())  #CategorySerializer()
     class Meta:
         model=Post
         fields=['id', 'author','title','content','snippet','category', 'status','relative_url','absolute_url', 'published_date' ]
@@ -24,3 +24,14 @@ class PostSerializer(serializers.ModelSerializer):
         request=self.context.get('request')
         return request.build_absolute_uri(obj.pk)
 
+    def to_representation(self, instance):
+        rep=super().to_representation(instance)
+        request=self.context.get('request')
+        if request.parser_context.get('kwargs').get('pk'):       #if pk be sended in the url.
+            rep.pop('snippet',None)
+            rep.pop('relative_url',None)
+            rep.pop('absolute_url',None)
+        else:
+            rep.pop('content',None)
+        rep['category']=CategorySerializer(instance.category).data         
+        return rep
