37ef8b50cc72af76ed7b88096b3ec39cb015ba98
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-11 12:14:28 +0330

    Customizing Token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from .views import *
-from rest_framework.authtoken.views import ObtainAuthToken
+#from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name='api-v1'
 
 urlpatterns = [
    #registration
    path('registration/', RegistrationApiView.as_view(),name='registration') ,
-   path('token/login/', ObtainAuthToken.as_view(),name='token-login') ,
+  # path('token/login/', ObtainAuthToken.as_view(),name='token-login') ,
+   path('token/login/', CustomObtainAuthToken.as_view(),name='token-login') ,
+   
 
    #change password
    #reset password
