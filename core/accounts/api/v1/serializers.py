b732fa4bec42baeccedf43cff900ff55d8520662
Arbz583 <bakhtiarialireza44@gmail.com>
2023-06-12 13:29:48 +0330

    Customizing JWT

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from ...models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password1=serializers.CharField(max_length=255, write_only=True)
@@ -20,3 +21,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password1', None)
         return User.objects.create_user(**validated_data)
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        validated_data=super().validate(attrs)
+        validated_data['email']=self.user.email
+        validated_data['user_id']=self.user.id
+        return validated_data
